customink/ClassyCAS

commit 52690abdb4f8d0f9b3455a4fca1aafea33bca2d4

Fixed a bug that would cause the service ticket to not validate if the service URL had a query string. So now, only the protocol, host, and port count towards validating the service URL.

lib/classy_cas.rb
@@ -56,14 +56,14 @@ module ClassyCAS
       @service_url = Addressable::URI.parse(params[:service])
       @renew = [true, "true", "1", 1].include?(params[:renew])
       @gateway = [true, "true", "1", 1].include?(params[:gateway])
-    
+
       if @renew
         @login_ticket = LoginTicket.create!(settings.redis)
         render_login
       elsif @gateway
         if @service_url
           if sso_session
-            st = ServiceTicket.new(params[:service], sso_session.username)
+            st = ServiceTicket.new( @service_url.omit(:query).to_s, sso_session.username)
             st.save!(settings.redis)
             redirect_url = @service_url.clone
             if @service_url.query_values.nil?
@@ -82,7 +82,7 @@ module ClassyCAS
       else
         if sso_session
           if @service_url
-            st = ServiceTicket.new(params[:service], sso_session.username)
+            st = ServiceTicket.new(@service_url.omit(:query).to_s, sso_session.username)
             st.save!(settings.redis)
             redirect_url = @service_url.clone
             if @service_url.query_values.nil?
@@ -100,27 +100,34 @@ module ClassyCAS
         end
       end
     end
-    
+
     post "/login" do
       username = params[:username]
       password = params[:password]
-    
-      service_url = params[:service]
-    
+
+      service_url = Addressable::URI.parse(params[:service])
+
       warn = [true, "true", "1", 1].include? params[:warn]
       # Spec is undefined about what to do without these params, so redirecting to credential requestor
       redirect "/login", 303 unless username && password && login_ticket
       # Failures will throw back to self, which we've registered with Warden to handle login failures
       warden.authenticate!(:scope => :cas, :action => 'unauthenticated')
-      
+
       tgt = TicketGrantingTicket.create!(username, settings.redis)
       cookie = tgt.to_cookie(request.host)
       response.set_cookie(*cookie)
-    
+
       if service_url && !warn
-        st = ServiceTicket.new(service_url, username)
+        st = ServiceTicket.new( service_url.omit(:query), username)
         st.save!(settings.redis)
-        redirect service_url + "?ticket=#{st.ticket}", 303
+
+        redirect_url = service_url.clone
+        if service_url.query_values.nil?
+          redirect_url.query_values = service_url.query_values = {:ticket => st.ticket}
+        else
+          redirect_url.query_values = service_url.query_values.merge(:ticket => st.ticket)
+        end
+        redirect redirect_url.to_s, 303
       else
         render_logged_in
       end

lib/service_ticket.rb
@@ -23,7 +23,10 @@ class ServiceTicket
   end
   
   def valid_for_service?(url)
-    service_url == url
+    url = Addressable::URI.parse(url)
+
+    # Don't include query strings in validation
+    service_url == url.omit(:query).to_s
   end
   
   def ticket

commit 4dc5b372cc1681ced2fb9e8c4418c8f1633867a5

Fixed some test failures I introduced in commit 52690ab

lib/classy_cas.rb
@@ -89,7 +89,7 @@ module ClassyCAS
       elsif @gateway
         if @service_url
           if sso_session
-            st = ServiceTicket.new( @service_url.omit(:query).to_s, sso_session.username)
+            st = ServiceTicket.new( @service_url, sso_session.username)
             st.save!(settings.redis)
             redirect_url = @service_url.clone
             if @service_url.query_values.nil?
@@ -108,7 +108,7 @@ module ClassyCAS
       else
         if sso_session
           if @service_url
-            st = ServiceTicket.new(@service_url.omit(:query).to_s, sso_session.username)
+            st = ServiceTicket.new(@service_url, sso_session.username)
             st.save!(settings.redis)
             redirect_url = @service_url.clone
             if @service_url.query_values.nil?
@@ -153,7 +153,7 @@ module ClassyCAS
       response.set_cookie(*cookie)
 
       if service_url && !warn
-        st = ServiceTicket.new( service_url.omit(:query), username)
+        st = ServiceTicket.new( service_url, username)
         st.save!(settings.redis)
 
         redirect_url = service_url.clone

lib/service_ticket.rb
@@ -1,3 +1,4 @@
+
 class ServiceTicket
   class << self
     def find!(ticket, store)
@@ -18,7 +19,9 @@ class ServiceTicket
   attr_reader :username, :service_url
   
   def initialize(service_url, username)
-    @service_url = service_url
+    service_url = Addressable::URI.parse(service_url) unless service_url.is_a?(Addressable::URI)
+
+    @service_url = normalize(service_url)
     @username = username
   end
   
@@ -26,7 +29,7 @@ class ServiceTicket
     url = Addressable::URI.parse(url)
 
     # Don't include query strings in validation
-    service_url == url.omit(:query).to_s
+    service_url == normalize(url)
   end
   
   def ticket
@@ -46,4 +49,11 @@ class ServiceTicket
     end
     
   end
+
+private
+
+  def normalize( url )
+    url.omit(:query).to_s
+  end
+
 end

test/protocol/cas_server_test.rb
@@ -405,23 +405,19 @@ class CasServerTest < Test::Unit::TestCase
             
           end
           should "redirect to /unauthorized but render /login" do
-            assert last_response.redirect?
-            follow_redirect!
             assert_have_selector "input[name='username']"
             assert_have_selector "input[name='password']"
             assert_have_selector "input[name='lt']"
           end
 
           should "preserve the service url in a hidden field" do
-            assert last_response.redirect?
-            follow_redirect!
             assert_have_selector "input[name='service']", :value => 'foo'
           end
           
           # RECOMMENDED
           # Will implement with some kind of flash message
           should "display an error message describing why login failed" do
-            assert_match /Login was not successful/, last_response.body
+            assert_have_selector "div.message.error p", :content => "Invalid username or password"
           end
 
           # RECOMMENDED
@@ -567,7 +563,7 @@ class CasServerTest < Test::Unit::TestCase
         end
 
         context "the ticket provided was valid, but the service specified did not match the service associated with the ticket" do
-          setup { get "/serviceValidate", :service => "http://example.com", :ticket => @st.ticket }
+          setup { get "/serviceValidate", :service => "http://example.org", :ticket => @st.ticket }
           should "respond with INVALID_SERVICE" do
 
             assert_invalid_service_xml_response(last_response)
@@ -666,7 +662,7 @@ class CasServerTest < Test::Unit::TestCase
           end
 
           context "the ticket provided was valid, but the service specified did not match the service associated with the ticket" do
-            setup { get "/proxyValidate", :service => "http://example.com", :ticket => @st.ticket }
+            setup { get "/proxyValidate", :service => "http://example.org", :ticket => @st.ticket }
             should "respond with INVALID_SERVICE" do
 
               assert_invalid_service_xml_response(last_response)
@@ -699,8 +695,8 @@ class CasServerTest < Test::Unit::TestCase
       # 3.1.1
       context "properties" do
         should "be valid only for the service that was specified to /login when they were generated" do
-          assert @st.valid_for_service?(@test_service_url)
-          assert !@st.valid_for_service?("http://google.com")
+          assert @st.valid_for_service?(@test_service_url), "#{@test_service_url} is not valid for the service but should be"
+          assert !@st.valid_for_service?("http://google.com"), "Is valid, but should not be"
         end
 
         should "not include the service identifier in the service ticket" do